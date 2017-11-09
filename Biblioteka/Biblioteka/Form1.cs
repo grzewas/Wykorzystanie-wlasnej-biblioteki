8ade1f43fc1c8cb7fb2659ec9c8323c9be94724e
grzewas <32064426+grzewas@users.noreply.github.com>
2017-11-09 10:53:31 +0100

    Add files via upload

---
+++ Biblioteka/Biblioteka/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Trojkaty;
+
+namespace Biblioteka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double b = double.Parse(textBox2.Text);
+                double c = double.Parse(textBox3.Text);
+                Trojkat ABC = new Trojkat(a, b, c);
+                label7.Text = Math.Round(ABC.KatB(), 2).ToString();
+                label8.Text = Math.Round(ABC.KatC(), 2).ToString();
+                label9.Text = Math.Round(ABC.KatA(), 2).ToString();
+                label12.Text = Math.Round(ABC.PoleTrojkata(), 3).ToString();
+                label13.Text = Math.Round(ABC.ObwodTrojkata(), 3).ToString();
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+        }
+
+    }
+}
